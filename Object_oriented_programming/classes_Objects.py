199b5554e5df6e17b7abbfdd8dac24627d6e0952
theseptember6th <shresthakristal42@gmail.com>
2024-05-26 17:23:01 +0545

    class object basic easy

---
+++ Object_oriented_programming/classes_Objects.py
@@ -0,0 +1,9 @@
+class kristal:
+    age=20
+    name="kristal"
+object1=kristal()  #syntax similar as function
+print(object1.name)
+object1.name="shrestha"
+print(object1.name)
+object1.college="lalitpur engineering college"   #can create new variables that is not present in class
+print(object1.college) 
