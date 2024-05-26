dd9e4d2f35346b3543bee377f293ae87e8747f7c
theseptember6th <shresthakristal42@gmail.com>
2024-05-26 18:39:13 +0545

    class methods decorator

--- Object_oriented_programming/class_methods.py
+++ Object_oriented_programming/class_methods.py
@@ -1,4 +1,4 @@
-#class methods that can change  the value of the class variables 
+#class methods is the methods that can be used by objects and that can change  the value of the class variables 
 
 class kristal:
     age=20
@@ -12,7 +12,12 @@ class kristal:
 object1=kristal()
 object1.age=10  #this changes the object's variables values and not of the generic class
 print(object1.age)
-object1.change_age(18)  #this will change the variable's value of the generic class
+object1.change_age(18)  #this will change the variable's value of the generic class by object
 
 object2=kristal()
+print(object2.age)
+
+
+#however you can directly change value of class using class
+kristal.age=30   
 print(object2.age)
