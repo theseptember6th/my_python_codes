ccbf90ca0d9a56b5ed4e9dbf504eb7c1143d5468
theseptember6th <shresthakristal42@gmail.com>
2024-05-26 21:20:46 +0545

    single inheritance easy

--- Object_oriented_programming/singleinheritance.py
+++ Object_oriented_programming/singleinheritance.py
@@ -1,23 +1,36 @@
 class Animal:
-    def __init__(self,name,species):
+    def __init__(self,name,species):#requires object to call
         print("from the base class")
         self.name=name
         self.species=species
         print("from the base class")
 
-    def sound():
+    def sound(self): #requires object to call
         print("sound by animal")
     
+   
+    def printdetails(self):
+        print(f"name={self.name},species={self.species}")
+
+    
 class Dog(Animal): #inherited from animal
-    def __init__(self,name,breed):
+    def __init__(self,name,breed,species):
         print("from the derived class")
         self.name=name
         self.breed=breed
-        self.species="dog"  #variable from parent
+        self.species=species  #variable from parent
 
     def sound(self):             #method overriding
-        Animal.sound()        #function from animal through class
+        Animal.sound(self)        #function from animal through class
         print("Bark!")
+    
+    
+    def printdetails(self):
+        Animal.printdetails(self)
+        print(f"name={self.name},species={self.species},breed={self.breed}")
 
-derived_object=Dog("tommy","germanshepherd")
-derived_object.sound()        
+derived_object=Dog("tommy","germanshepherd","dog")
+#derived_object1=Dog("doggy","bulldog") #error unmatched derived constructor
+derived_object.printdetails()
+derived_object.sound()
+   
