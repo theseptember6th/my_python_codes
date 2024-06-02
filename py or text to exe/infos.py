fa38b73d3fd7870d44f875d5ebc44c9a854bf6bb
kristal <shresthakristal42@gmail.com>
2024-06-02 20:29:14 +0545

    python codes or text written code to exe files

---
+++ py or text to exe/infos.py
@@ -0,0 +1,3 @@
+# We create lots of Python programs and want to share them with the world. If we want our python program to run on any computer without the IDE or even installing Python itself, we must convert it to .exe. All the apps and programs we use on our computer are written using some language or code, but we do not have to install the particular language for the program to run.
+
+#run code from main.text
